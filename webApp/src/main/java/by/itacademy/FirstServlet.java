b33fbba783bda48a58a407e17e9b194b850411e0
Ivan <semen_gen@tut.by>
2020-08-07 15:57:52 +0300

    Сделал вычисление в первом сервлете по GET запросу

--- webApp/src/main/java/by/itacademy/FirstServlet.java
+++ webApp/src/main/java/by/itacademy/FirstServlet.java
@@ -15,6 +15,46 @@ public class FirstServlet extends HttpServlet {
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    response.getWriter().println("FIRST");
+    double firstNum, secondNum;
+    Double result = null;
+    String operation;
+
+    try {
+      firstNum = Double.parseDouble(request.getParameter("firstNum"));
+      secondNum = Double.parseDouble(request.getParameter("secondNum"));
+      operation = request.getParameter("operation");
+
+      SimpleCalc calc = new SimpleCalc();
+
+      switch (operation) {
+        case "plus":
+          result = calc.sum(firstNum, secondNum);
+          operation = "+";
+          break;
+        case "minus":
+          result = calc.minus(firstNum, secondNum);
+          operation = "-";
+          break;
+        case "mul":
+          result = calc.mul(firstNum, secondNum);
+          operation = "*";
+          break;
+        case "div":
+          result = calc.div(firstNum, secondNum);
+          operation = "/";
+          break;
+      }
+
+      if (result != null) {
+        response.getWriter()
+            .println(firstNum + " " + " " + operation + " " + " " + secondNum + " = " + result);
+      } else {
+        response.setCharacterEncoding("windows-1251");
+        response.getWriter().println("Не удалось вычислить");
+      }
+
+    } catch (Exception e) {
+      System.out.println("Error of parameters: " + e.getMessage());
+    }
   }
 }
