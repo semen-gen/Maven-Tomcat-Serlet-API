a8c446667e020fa19a857ca7a73f1e75c790cbd8
Ivan <semen_gen@tut.by>
2020-08-07 21:04:26 +0300

    Сделал второй сервлет через пост и проброс гет метода к первому сервлету

--- webApp/src/main/java/by/itacademy/SecondServlet.java
+++ webApp/src/main/java/by/itacademy/SecondServlet.java
@@ -1,22 +1,66 @@
 package by.itacademy;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "SecondServlet", urlPatterns = "/second")
+@WebServlet(name = "SecondServlet", urlPatterns = "/second", loadOnStartup = 1)
 public class SecondServlet extends HttpServlet {
 
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
+    BigDecimal firstNum, secondNum, result = null;
+    String operation;
 
+    try {
+      firstNum = new BigDecimal(request.getParameter("firstNum"));
+      secondNum = new BigDecimal(request.getParameter("secondNum"));
+      operation = request.getParameter("operation");
+
+      SimpleCalc calc = new SimpleCalc();
+
+      switch (operation) {
+        case "plus":
+          result = calc.sum(firstNum, secondNum);
+          operation = "+";
+          break;
+        case "minus":
+          result = calc.minus(firstNum, secondNum);
+          operation = "-";
+          break;
+        case "mul":
+          result = calc.mul(firstNum, secondNum);
+          operation = "*";
+          break;
+        case "div":
+          result = calc.div(firstNum, secondNum);
+          operation = "/";
+          break;
+      }
+
+      if (result != null) {
+        response.getWriter()
+            .println(firstNum + " " + operation + " " + secondNum + " = " + result);
+      } else {
+        response.setCharacterEncoding("windows-1251");
+        response.getWriter().println("Не удалось вычислить");
+      }
+
+    } catch (Exception e) {
+      response.getWriter().println("Error of parameters: " + e.getMessage());
+    }
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    response.getWriter().println("SECOND");
+    ServletContext context = getServletContext();
+    RequestDispatcher dispatcher = context.getRequestDispatcher("/first");
+    dispatcher.forward(request, response);
   }
 }
