79c20c098beb8bd3d8c049b067eded89a3a4bff7
Ivan <semen_gen@tut.by>
2020-08-07 14:12:58 +0300

    Создал модуль с калькулятором

---
+++ service/src/main/java/by/itacademy/SimpleCalc.java
@@ -0,0 +1,21 @@
+package by.itacademy;
+
+public class SimpleCalc {
+
+  public double sum(double a, double b) {
+    return a + b;
+  }
+
+  public double minus(double a, double b) {
+    return a - b;
+  }
+
+  public double mul(double a, double b) {
+    return a * b;
+  }
+
+  public double div(double a, double b) {
+    return a / b;
+  }
+
+}
