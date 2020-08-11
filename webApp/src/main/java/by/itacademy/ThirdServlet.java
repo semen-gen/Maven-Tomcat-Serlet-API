221bcef639741b2923024eb2857c0ff11d6dda38
Ivan <semen_gen@tut.by>
2020-08-11 15:33:33 +0300

    Переделал сервлет с ошибкой и скрины

--- webApp/src/main/java/by/itacademy/ThirdServlet.java
+++ webApp/src/main/java/by/itacademy/ThirdServlet.java
@@ -1,9 +1,6 @@
 package by.itacademy;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,33 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "ThirdServlet", urlPatterns = "/third")
 public class ThirdServlet extends HttpServlet {
 
-  LocalDateTime timestamp;
-  final DateTimeFormatter FORMATTER;
-  private static final Logger LOG = Logger.getLogger(ThirdServlet.class.getName());
-
-  {
-    FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-  }
-
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    timestamp = LocalDateTime.now();
-
-    try {
-      throw new RuntimeException("timestamp: " + timestamp.format(FORMATTER));
-    } catch (RuntimeException e) {
-      LOG.info( e.getMessage());
-    }
+    myException();
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    timestamp = LocalDateTime.now();
+    myException();
+  }
 
-    try {
-      throw new RuntimeException("timestamp: " + timestamp.format(FORMATTER));
-    } catch (RuntimeException e) {
-      LOG.info( e.getMessage());
-    }
+  void myException() {
+    throw new RuntimeException("Test error");
   }
 }
